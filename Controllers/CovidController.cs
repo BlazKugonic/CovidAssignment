9d5f48b9df6e0b718ca04d522cf58715dd44c60f
Blaz <blaz.kugonic@student.um.si>
2021-09-23 10:56:22 +0200

    Add project files.

---
+++ Controllers/CovidController.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System.Text.Json;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
+using CsvHelper;
+using Microsoft.VisualBasic.FileIO;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using System.Globalization;
+using System.Collections;
+
+namespace CovidAssignment.Controllers
+{
+
+
+    [ApiController]
+    [Route("api")]
+    public class CovidController : ControllerBase
+    {
+        //some simple authentication
+        public string user = "covid";
+        public string pass = "2021";
+        public List<Covid> Getdata()
+        {
+            //read csv with streamreader, with header
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            };
+            using (var reader = new StreamReader(@".\region-cases.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+
+            {
+                var records = csv.GetRecords<Covid>().ToList();
+                return records;
+            }
+
+
+        }
+
+        private readonly ILogger<CovidController> _logger;
+
+        public CovidController(ILogger<CovidController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult MyAction()
+        {
+
+            Getdata();
+
+            return Ok("Welcome to Covid API\n"+
+                "available apis:\n" +
+                "1. /region/cases (Region,From, To)\n" +
+                "2./region/lastweek");
+        }
+        //pridobivanje datuma (yyyy/MM/DD) in regije 
+        [HttpGet("region/cases")]
+        public IEnumerable Cases(string Region, DateTime From, DateTime To,  string username,string password)
+        {
+            if (username == user && password == pass)
+            {
+                List<Covid> covid = Getdata();
+                IList<string> Izpis = new List<string>();
+                foreach (var x in covid)
+                {
+                    if (x.date >= From && x.date <= To)
+                    {
+                        //Region, possible values (LJ, CE, KR, NM, KK, KP, MB, MS, NG, PO, SG, ZA)
+                        if (Region == "LJ")
+                        {
+                            System.Diagnostics.Debug.WriteLine("Date:", x.date);
+                            Izpis.Add("Date:" + x.date);
+                            System.Diagnostics.Debug.WriteLine("Number of active cases per day: " + x.regionljcasesactive);
+                            Izpis.Add("Number of active cases per day: " + x.regionljcasesactive);
+                            System.Diagnostics.Debug.WriteLine("Number of vaccinated 1st: " + x.regionljvaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionljvaccinated1sttodate);
+                            System.Diagnostics.Debug.WriteLine("Number of vaccinated 2nd: " + x.regionljvaccinated2ndtodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionljvaccinated2ndtodate);
+                            System.Diagnostics.Debug.WriteLine("Deceased to date: " + x.regionljdeceasedtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionljdeceasedtodate);
+                        }
+                        else if (Region == "CE")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regioncecasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regioncevaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regioncevaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regioncedeceasedtodate);
+                        }
+                        else if (Region == "KR")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionkrcasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionkrvaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionkrvaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionkrdeceasedtodate);
+                        }
+                        else if (Region == "NM")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionnmcasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionnmvaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionnmvaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionnmdeceasedtodate);
+                        }
+                        else if (Region == "KK")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionkkcasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionkkvaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionkkvaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionkkdeceasedtodate);
+                        }
+                        else if (Region == "KP")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionkpcasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionkpvaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionkpvaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionkpdeceasedtodate);
+                        }
+                        else if (Region == "MB")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionmbcasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionmbvaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionmbvaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionmbdeceasedtodate);
+                        }
+                        else if (Region == "MS")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionmscasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionmsvaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionmsvaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionmsdeceasedtodate);
+                        }
+                        else if (Region == "NG")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionngcasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionngvaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionngvaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionngdeceasedtodate);
+                        }
+                        else if (Region == "PO")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionpocasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionpovaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionpovaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionpodeceasedtodate);
+                        }
+                        else if (Region == "SG")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionsgcasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionsgvaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionsgvaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionsgdeceasedtodate);
+                        }
+                        else if (Region == "ZA")
+                        {
+                            Izpis.Add("Date:" + x.date);
+                            Izpis.Add("Number of active cases per day: " + x.regionzacasesactive);
+                            Izpis.Add("Number of vaccinated 1st:  " + x.regionzavaccinated1sttodate);
+                            Izpis.Add("Number of vaccinated 2nd:  " + x.regionzavaccinated2ndtodate);
+                            Izpis.Add("Deceased to date:  " + x.regionzadeceasedtodate);
+                        }
+                        else
+                        {
+                            // Izpis.Add("Ta regija ne obstaja!\n" + "Možne regije LJ, CE, KR, NM, KK, KP, MB, MS, NG, PO, SG, ZA");
+                            yield return StatusCode(500, "Ta regija ne obstaja!\n" + "Možne regije LJ, CE, KR, NM, KK, KP, MB, MS, NG, PO, SG, ZA");
+                            break;
+                        }
+                    }
+                    else if (From > To)
+                    {
+
+                        yield return StatusCode(500, "Datum ni pravilen. Zamenjajte vrsti red");
+                        break;
+                    }
+
+                }
+                yield return Izpis;
+            }
+            else
+            {
+                yield return StatusCode(200, "Wrong password or username!");
+            }
+            
+        }
+        [HttpGet("region/lastweek")]
+        public IEnumerable lastweekcases(string username, string password)
+        {
+            if (username == user && password == pass)
+            {
+                List<Covid> covid = Getdata();
+                // LJ, CE, KR, NM, KK, KP, MB, MS, NG, PO, SG, ZA"
+                int? LJ = 0, CE = 0, KR = 0, NM = 0, KK = 0, KP = 0, MB = 0, MS = 0, NG = 0, PO = 0, SG = 0, ZA = 0;
+                List<(string, int?)> Izpis = new List<(string, int?)>();
+                List<string> izpis2 = new List<string>();
+                //7dnevni seštevek v vsaki regiji
+                var LastsevenDays = covid.Skip(Math.Max(0, covid.Count() - 7)).Take(7);
+                foreach (var item in LastsevenDays)
+                {
+                    LJ += item.regionljcasesactive;
+                    CE += item.regioncecasesactive;
+                    KR += item.regionkrcasesactive;
+                    NM += item.regionnmcasesactive;
+                    KK += item.regionkkcasesactive;
+                    KP += item.regionkpcasesactive;
+                    MB += item.regionmbcasesactive;
+                    MS += item.regionngcasesactive;
+                    PO += item.regionpocasesactive;
+                    SG += item.regionsgcasesactive;
+                    ZA += item.regionzacasesactive;
+                    NG += item.regionngcasesactive;
+                    System.Diagnostics.Debug.WriteLine("LJ število aktivnih ", item.regionljcasesactive);
+                }
+                //System.Diagnostics.Debug.WriteLine("LJ število aktivnih ", LJ);
+                Izpis.Add((" sum of number of active cases LJ:", LJ));
+                Izpis.Add(("sum of number of active cases CE:", CE));
+                Izpis.Add(("sum of number of active cases KR:", KR));
+                Izpis.Add(("sum of number of active cases NM:", NM));
+                Izpis.Add(("sum of number of active cases KK:", KK));
+                Izpis.Add(("sum of number of active cases KP:", KP));
+                Izpis.Add(("sum of number of active cases MB:", MB));
+                Izpis.Add(("sum of number of active cases MS:", MS));
+                Izpis.Add(("sum of number of active cases PO:", PO));
+                Izpis.Add(("sum of number of active cases SG:", SG));
+                Izpis.Add(("sum of number of active cases ZA:", ZA));
+                Izpis.Add(("sum of number of active cases NG:", NG));
+                var newList = Izpis.OrderByDescending(x => x.Item2).ToList();
+                string temp;
+                foreach (var i in newList)
+                {
+                    temp = i.ToString();
+                    Console.WriteLine(temp);
+                    izpis2.Add(temp);
+                }
+               yield return izpis2;
+            }
+            else
+            {
+               yield return StatusCode(200, "Wrong password or username!");
+            }
+            
+        }
+    }
+}
---
+++ Covid.cs
@@ -0,0 +1,271 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidAssignment
+{
+    public class Covid
+    {
+        [Name("date")]
+        public DateTime date { get; set; }
+        [Name("regioncecasesactive")]
+        public int? regioncecasesactive { get; set; }
+        [Name("regioncecasesconfirmedtodate")]
+        public int? regioncecasesconfirmedtodate { get; set; }
+        [Name("regioncedeceasedtodate")]
+        public int? regioncedeceasedtodate { get; set; }
+        [Name("regioncevaccinated1sttodate")]
+        public int? regioncevaccinated1sttodate { get; set; }
+        [Name("regioncevaccinated2ndtodate")]
+        public int? regioncevaccinated2ndtodate { get; set; }
+        [Name("regionforeigncasesactive")]
+        public int? regionforeigncasesactive { get; set; }
+        [Name("regionforeigncasesconfirmedtodate")]
+        public int? regionforeigncasesconfirmedtodate { get; set; }
+        [Name("regionforeigndeceasedtodate")]
+        public int? regionforeigndeceasedtodate { get; set; }
+        [Name("regionkkcasesactive")]
+        public int? regionkkcasesactive { get; set; }
+        [Name("regionkkcasesconfirmedtodate")]
+        public int? regionkkcasesconfirmedtodate { get; set; }
+        [Name("regionkkdeceasedtodate")]
+        public int? regionkkdeceasedtodate { get; set; }
+        [Name("regionkkvaccinated1sttodate")]
+        public int? regionkkvaccinated1sttodate { get; set; }
+        [Name("regionkkvaccinated2ndtodate")]
+        public int? regionkkvaccinated2ndtodate { get; set; }
+        [Name("regionkpcasesactive")]
+        public int? regionkpcasesactive { get; set; }
+        [Name("regionkpcasesconfirmedtodate")]
+        public int? regionkpcasesconfirmedtodate { get; set; }
+        [Name("regionkpdeceasedtodate")]
+        public int? regionkpdeceasedtodate { get; set; }
+        [Name("regionkpvaccinated1sttodate")]
+        public int? regionkpvaccinated1sttodate { get; set; }
+        [Name("regionkpvaccinated2ndtodate")]
+        public int? regionkpvaccinated2ndtodate { get; set; }
+        [Name("regionkrcasesactive")]
+        public int? regionkrcasesactive { get; set; }
+        [Name("regionkrcasesconfirmedtodate")]
+        public int? regionkrcasesconfirmedtodate { get; set; }
+        [Name("regionkrdeceasedtodate")]
+        public int? regionkrdeceasedtodate { get; set; }
+        [Name("regionkrvaccinated1sttodate")]
+        public int? regionkrvaccinated1sttodate { get; set; }
+        [Name("regionkrvaccinated2ndtodate")]
+        public int? regionkrvaccinated2ndtodate { get; set; }
+        [Name("regionljcasesactive")]
+        public int? regionljcasesactive { get; set; }
+        [Name("regionljcasesconfirmedtodate")]
+        public int? regionljcasesconfirmedtodate { get; set; }
+        [Name("regionljdeceasedtodate")]
+        public int? regionljdeceasedtodate { get; set; }
+        [Name("regionljvaccinated1sttodate")]
+        public int? regionljvaccinated1sttodate { get; set; }
+        [Name("regionljvaccinated2ndtodate")]
+        public int? regionljvaccinated2ndtodate { get; set; }
+        [Name("regionmbcasesactive")]
+        public int? regionmbcasesactive { get; set; }
+        [Name("regionmbcasesconfirmedtodate")]
+        public int? regionmbcasesconfirmedtodate { get; set; }
+        [Name("regionmbdeceasedtodate")]
+        public int? regionmbdeceasedtodate { get; set; }
+        [Name("regionmbvaccinated1sttodate")]
+        public int? regionmbvaccinated1sttodate { get; set; }
+        [Name("regionmbvaccinated2ndtodate")]
+        public int? regionmbvaccinated2ndtodate { get; set; }
+        [Name("regionmscasesactive")]
+        public int? regionmscasesactive { get; set; }
+        [Name("regionmscasesconfirmedtodate")]
+        public int? regionmscasesconfirmedtodate { get; set; }
+        [Name("regionmsdeceasedtodate")]
+        public int? regionmsdeceasedtodate { get; set; }
+        [Name("regionmsvaccinated1sttodate")]
+        public int? regionmsvaccinated1sttodate { get; set; }
+        [Name("regionmsvaccinated2ndtodate")]
+        public int? regionmsvaccinated2ndtodate { get; set; }
+        [Name("regionngcasesactive")]
+        public int? regionngcasesactive { get; set; }
+        [Name("regionngcasesconfirmedtodate")]
+        public int? regionngcasesconfirmedtodate { get; set; }
+        [Name("regionngdeceasedtodate")]
+        public int? regionngdeceasedtodate { get; set; }
+        [Name("regionngvaccinated1sttodate")]
+        public int? regionngvaccinated1sttodate { get; set; }
+        [Name("regionngvaccinated2ndtodate")]
+        public int? regionngvaccinated2ndtodate { get; set; }
+        [Name("regionnmcasesactive")]
+        public int? regionnmcasesactive { get; set; }
+        [Name("regionnmcasesconfirmedtodate")]
+        public int? regionnmcasesconfirmedtodate { get; set; }
+        [Name("regionnmdeceasedtodate")]
+        public int? regionnmdeceasedtodate { get; set; }
+        [Name("regionnmvaccinated1sttodate")]
+        public int? regionnmvaccinated1sttodate { get; set; }
+        [Name("regionnmvaccinated2ndtodate")]
+        public int? regionnmvaccinated2ndtodate { get; set; }
+        [Name("regionpocasesactive")]
+        public int? regionpocasesactive { get; set; }
+        [Name("regionpocasesconfirmedtodate")]
+        public int? regionpocasesconfirmedtodate { get; set; }
+        [Name("regionpodeceasedtodate")]
+        public int? regionpodeceasedtodate { get; set; }
+        [Name("regionpovaccinated1sttodate")]
+        public int? regionpovaccinated1sttodate { get; set; }
+        [Name("regionpovaccinated2ndtodate")]
+        public int? regionpovaccinated2ndtodate { get; set; }
+        [Name("regionsgcasesactive")]
+        public int? regionsgcasesactive { get; set; }
+        [Name("regionsgcasesconfirmedtodate")]
+        public int? regionsgcasesconfirmedtodate { get; set; }
+        [Name("regionsgdeceasedtodate")]
+        public int? regionsgdeceasedtodate { get; set; }
+        [Name("regionsgvaccinated1sttodate")]
+        public int? regionsgvaccinated1sttodate { get; set; }
+        [Name("regionsgvaccinated2ndtodate")]
+        public int? regionsgvaccinated2ndtodate { get; set; }
+        [Name("regionunknowncasesactive")]
+        public int? regionunknowncasesactive { get; set; }
+        [Name("regionunknowncasesconfirmedtodate")]
+        public int? regionunknowncasesconfirmedtodate { get; set; }
+        [Name("regionunknowndeceasedtodate")]
+        public int? regionunknowndeceasedtodate { get; set; }
+        [Name("regionzacasesactive")]
+        public int? regionzacasesactive { get; set; }
+        [Name("regionzacasesconfirmedtodate")]
+        public int? regionzacasesconfirmedtodate { get; set; }
+        [Name("regionzadeceasedtodate")]
+        public int? regionzadeceasedtodate { get; set; }
+        [Name("regionzavaccinated1sttodate")]
+        public int? regionzavaccinated1sttodate { get; set; }
+        [Name("regionzavaccinated2ndtodate")]
+        public int? regionzavaccinated2ndtodate { get; set; }
+
+    }
+    
+}
+
+public class Unknown
+        {
+        }
+
+        public class Sg
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Ce
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Kk
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Lj
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Za
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Mb
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Kr
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Foreign
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Ms
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Po
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Ng
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Nm
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Kp
+        {
+            public int? activeCases { get; set; }
+            public int? confirmedToDate { get; set; }
+            public int? deceasedToDate { get; set; }
+        }
+
+        public class Regions
+        {
+            public Unknown Unknown { get; set; }
+            public Sg Sg { get; set; }
+            public Ce Ce { get; set; }
+            public Kk Kk { get; set; }
+            public Lj Lj { get; set; }
+            public Za Za { get; set; }
+            public Mb Mb { get; set; }
+            public Kr Kr { get; set; }
+            public Foreign Foreign { get; set; }
+            public Ms Ms { get; set; }
+            public Po Po { get; set; }
+            public Ng Ng { get; set; }
+            public Nm Nm { get; set; }
+            public Kp Kp { get; set; }
+        }
+
+        public class Root
+        {
+            public int? Year { get; set; }
+            public int? Month { get; set; }
+            public int? Day { get; set; }
+            public Regions Regions { get; set; }
+        }
+
+ 
